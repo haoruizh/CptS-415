b98dbbde87eeedb2a0f60e165551c81fabd551f4
Biuuubiubiu <74947167+Biuuubiubiu@users.noreply.github.com>
2020-11-27 21:35:38 -0800

    Update 415main.py

--- project/415main.py
+++ project/415main.py
@@ -1,4 +1,7 @@
 import sys
+from sequentialAlgorithm import *
+from dataProcess import *
+
 def main():
     #选择跑哪个function
     #菜单
@@ -6,7 +9,7 @@ def main():
     print("This is Cpts415 group 2 project")
     print("Main Menu")
     print("----------")
-    print("1: AAA")
+    print("1: Simple sequential sql")
     print("2: BBB")
     print("3: CCC")
     print("4: AAA")
@@ -15,7 +18,13 @@ def main():
 
     user_option = input("What do you want to do?")
     if user_option == '1':
-        print("Hi")                 # function 1
+        print("----------Simple sequential sql-----------")                 # function 1
+        user_file = input("Please print the file name you want to work with:")
+        user_M = input("How many value do you want to get?")
+        user_conditions = input("Please give me the conditions you want:")
+        user_select = input("Which attribute do you want to work with?")
+
+        sqAlg1(user_file, user_M, user_conditions, user_select)
     if user_option == '2':
         print("Hello")              # function 2
     if user_option == '3':
